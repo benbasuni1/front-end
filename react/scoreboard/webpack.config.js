6c72d8c876e2a2de4ade47dd1d4b6885bbf60906
Ben Basuni <benbasuni1@gmail.com>
2018-03-30 11:30:15 -0700

    got scoreboard to modularize from app to component

--- react/scoreboard/webpack.config.js
+++ react/scoreboard/webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
       {
         test    : /\.js$/,
         exclude : /node_modules/,
-        loader  : 'babel-loader'
+        loader : 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   }
